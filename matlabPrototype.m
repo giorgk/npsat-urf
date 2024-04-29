c5170697d2a016bb777031b763138e4e53ce95a9
Georgios Kourakos <giorgk@gmail.com>
2024-04-29 02:10:19 -0700

    Update matlabPrototype.m

--- matlabPrototype.m
+++ matlabPrototype.m
@@ -1,13 +1,25 @@
 %% Program Options
 alpha = 0.32;
 beta = 0.83;
+minStep = 1;
 
 
 %% First we read the streamlines.
 S = readICHNOSgather('test_data/teststrmlinfit.traj');
 %% Simulate one streamline
 ii = 1;
+pp = S(ii,1).p;
+vv = S(ii,1).v;
 %% 
+% Calculate cumulative length
+xp = zeros(size(pp,1),1);
+vp = zeros(size(pp,1),1);
+for k = 1:size(pp,1)-1
+    stepLen = sqrt(sum((pp(k,:) - pp(k+1,:)).^2));
+    if stepLen > minStep
+        xp(k+1) = xp(k) + stepLen;
+    end
+end
 
 
 
