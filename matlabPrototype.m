9927bef9ab0d574731be0560f0fdd0b38d885131
Giorgos Kourakos <giorgk@gmail.com>
2024-04-29 11:56:37 +0300

    Start Matlab script

---
+++ matlabPrototype.m
@@ -0,0 +1,14 @@
+%% Program Options
+alpha = 0.32;
+beta = 0.83;
+
+
+%% First we read the streamlines.
+S = readICHNOSgather('test_data/teststrmlinfit.traj');
+%% Simulate one streamline
+ii = 1;
+%% 
+
+
+
+
