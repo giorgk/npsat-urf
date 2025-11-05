df03274376e348ea486a642c77f4ad63269dc7ba
Georgios Kourakos <giorgk@gmail.com>
2025-11-05 02:46:14 -0800

    start rust project

---
+++ RUST/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    dbg!(&args);
+    // let query = &args[1];
+    // let file_path = &args[2];
+    // println!("Searching for {query} in {file_path}");
+    println!("Hello, world!");
+}
