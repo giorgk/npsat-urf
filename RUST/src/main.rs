85144620a9e998cd8d5a4bf084b524df130a5f80
Giorgos Kourakos <giorgk@gmail.com>
2025-11-05 23:50:24 +0200

    Update main.rs

--- RUST/src/main.rs
+++ RUST/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    dbg!(args.len());
+    if (args.len() < 2){
+        panic!("Not enough arguments");
+    }
+    else{
+        let query = &args[1];
+        println!("Input file {query}");
+    }
     dbg!(&args);
     // let query = &args[1];
     // let file_path = &args[2];
